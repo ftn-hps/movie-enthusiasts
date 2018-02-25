f8bef00dcbf9d1f8cab78ec0b8fd0ebeaa75af4f
BojanStipic <bojanstipic@gmail.com>
2018-02-25 12:50:46 +0100

    Spring Boot project

---
+++ src/test/java/ftnhps/movieenthusiasts/MovieEnthusiastsApplicationTests.java
@@ -0,0 +1,16 @@
+package ftnhps.movieenthusiasts;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MovieEnthusiastsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
