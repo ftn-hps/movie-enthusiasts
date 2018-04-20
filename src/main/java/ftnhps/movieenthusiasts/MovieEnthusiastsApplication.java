8a35717154520c3756ef88b022d8f3394b434680
BojanStipic <bojanstipic@gmail.com>
2018-04-20 18:21:29 +0200

    Email utils

--- src/main/java/ftnhps/movieenthusiasts/MovieEnthusiastsApplication.java
+++ src/main/java/ftnhps/movieenthusiasts/MovieEnthusiastsApplication.java
@@ -2,10 +2,12 @@ package ftnhps.movieenthusiasts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class MovieEnthusiastsApplication {
 
 	public static void main(String[] args) {
