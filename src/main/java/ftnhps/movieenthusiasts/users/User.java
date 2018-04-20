18e194040b001b1db19ec2e99f1600ff97d0d85f
BojanStipic <bojanstipic@gmail.com>
2018-04-20 20:05:13 +0200

    Rename USER table
    PostgreSQL can not have a USER table, so it has to be renamed.

--- src/main/java/ftnhps/movieenthusiasts/users/User.java
+++ src/main/java/ftnhps/movieenthusiasts/users/User.java
@@ -7,6 +7,7 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -19,6 +20,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
 @Entity
+@Table(name = "CUSTOM_USER")
 public class User {
 
 	@Id
