b0d63c4df22f5e8e95ba69f27fc9633d20a199c8
Nebojsa Horvat <horvatnebojsa@yahoo.com>
2018-04-20 20:55:04 +0200

    tests

--- src/test/java/ftnhps/movieenthusiasts/projectiosTest/ProjectionServiceTest.java
+++ src/test/java/ftnhps/movieenthusiasts/projectiosTest/ProjectionServiceTest.java
@@ -9,10 +9,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
+import ftnhps.movieenthusiasts.places.Place;
 import ftnhps.movieenthusiasts.projections.Projection;
 import ftnhps.movieenthusiasts.projections.ProjectionService;
+import ftnhps.movieenthusiasts.users.UserService;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,10 +27,54 @@ public class ProjectionServiceTest {
 
 	@Autowired
 	private ProjectionService projectionService;
+	@Autowired
+	private UserService userService;
+	
 	
 	@Test
 	public void testFindAll() {
 		List<Projection> students = projectionService.findAll();
-		assertThat(students).hasSize(DB_COUNT_PROJECTIONS); 
+		assertThat(students).hasSize(DB_COUNT_PROJECTIONS +1); 
+	}
+	
+	@Test
+	public void TestFindOne()
+	{
+		Projection projection = projectionService.findOne(new Long(DB_ID));
+		
+		assertThat(projection).isNotNull();
+		assertThat(projection.getName()).isEqualTo(DB_NAME);
+	}
+	
+	@Test
+    @Transactional
+    @Rollback(true) 
+	public void testAdd() {
+		Place place = new Place();
+		place .setId(new Long(1));
+		Projection projection1 = new Projection(place,
+				"Test",
+				"Test",
+				"Drama", 
+				"Test",
+				120, 
+				"/img/placeholder.png",
+				3, 
+				"Test",
+				333.00);
+		
+		int sizeBefore =  projectionService.findAll().size();
+		
+		Projection projection = projectionService.add(projection1);
+		assertThat(projection).isNotNull();
+		
+		
+		List<Projection> projections = projectionService.findAll();
+		assertThat(projections).hasSize(sizeBefore+1);
+		
+		projection = projectionService.findOne(projection.getId());
+		assertThat(projection.getName()).isEqualTo("Test");
+		assertThat(projection.getGenre()).isEqualTo("Drama");
+		
 	}
 }
--- src/test/java/ftnhps/movieenthusiasts/testingConstants/ProjectionConstants.java
+++ src/test/java/ftnhps/movieenthusiasts/testingConstants/ProjectionConstants.java
@@ -2,5 +2,7 @@ package ftnhps.movieenthusiasts.testingConstants;
 
 public class ProjectionConstants {
 
-	public static final int DB_COUNT_PROJECTIONS  = 3;
+	public static final int DB_COUNT_PROJECTIONS = 3;
+	public static final int DB_ID = 1;
+	public static final String DB_NAME = "R&J";
 }
